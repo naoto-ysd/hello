e8287c45dd7045929326d18a25694d81b1de9db1
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2022-11-06 21:17:49 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container">
+      <p>{{message}}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      message: 'hello world'
+    }
+  }
+}
+</script>
---
+++ pages/price.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="container">
+    <h1>
+      Price page
+    </h1>
+  </section>
+</template>
